5720ebcf226a1306b3c109beef91a98cea3e7545
Adrien Reynard <reynard.adrien.08@gmail.com>
2026-03-04 08:22:54 +0100

    Optimise Dockerfile and remove unused dependencies - Closes #57

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@myriaddreamin/typst-ts-node-compiler'],
+  output: 'standalone',
 };
 
 export default nextConfig;
