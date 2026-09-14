4ad1f9eadb44d37352cf097f7ad7024985ebed73
Adrien Reynard <reynard.adrien.08@gmail.com>
2026-09-14 08:44:17 +0200

    fix: eslint in zoom hook

--- app/src/hooks/useZoom.js
+++ app/src/hooks/useZoom.js
@@ -56,7 +56,7 @@ export function useZoomWatcher() {
       if (refs.btnZoomIn) refs.btnZoomIn.onclick = null;
       if (refs.btnZoomOut) refs.btnZoomOut.onclick = null;
     };
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 }
 
 // ----------------------------------------
