ff333d531775629f868353e578ebdeb58284c7d8
Adrien Reynard <reynard.adrien.08@gmail.com>
2026-09-17 14:44:38 +0200

    feat: add search bar in docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -46,6 +46,21 @@ const config: Config = {
     ],
   ],
 
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        language: ['en'],
+        indexDocs: true,
+        indexPages: false,
+        docsRouteBasePath: '/',
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+      },
+    ],
+  ],
+
   themeConfig: {
     image: 'img/banner.jpg',
 
